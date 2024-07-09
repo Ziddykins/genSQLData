14e03472fda009639b7465bf92ba7853938a3257
Jeff <Jeff0012@hotmail.com>
2024-07-08 21:07:10 -0400

    Update genSQLData.pl

--- genSQLData.pl
+++ genSQLData.pl
@@ -7,12 +7,17 @@ use utf8;
 use Getopt::Long;
 
 my ($count, $table);
+my ($first, $last, $ipsum);
+
 my @columns;
 
 GetOptions(
   'columns|c=s{1,}' => \@columns,
   'count|r=i'       => \$count,
   'table|t=s'       => \$table,
+  'first|f=s'       => \$first,
+  'last|l=s'        => \$last,
+  'ipsum|i=s'       => \$ipsum,
   'help|?+'         => sub { help() },
 );
 
@@ -21,6 +26,10 @@ if (scalar @ARGV < 1) {
     die;
 }
 
+$first //= 'first.txt';
+$last  //= 'last.txt';
+$ipsum //= 'ipsum.txt';
+
 sub help {
     print <<EOF;
 - MySQL Example Data Generator -
@@ -28,55 +37,80 @@ Usage:
     ./$0 --table <table> --count <#rows> --columns <column1 column2> <identifiers>
 
     Identifiers:
-        I    - Integer - Number between 1 and 5000
-        B    - Boolean - 'True' or 'False'
-        D    - Date - 'YYYY-MM-DD hh:mm:ss' format
-        F    - First name, picks a random one
-        L    - Last name, picks a random one
-        N    - NULL
-        P    - Phone number - xxx-xxx-xxxx
-        E    - Random first name and random word from lorem for domain
-        C    - Currency - format of #.##
-        IP   - IP address - May be private
-        PW   - Random upper-cased password of length 10
-        CC:* - Credit card - randomly generated CC, probably not valid
-                 - Args: MC for mastercard, V for Visa, DI for Discover
-        V:*  - A randomly generated sentence of # words long
-                 - Args: <number> for number of words
+        I[:#]        - Integer - Number between 1 and 5000
+        B            - Boolean - 'True' or 'False'
+        M            - Currency in the format of #.##
+        D[:T]        - Date - 'YYYY-MM-DD hh:mm:ss' format
+        F            - First name, picks a random one
+        L            - Last name, picks a random one
+        N            - NULL
+        P            - Phone number - xxx-xxx-xxxx
+        E            - Email - Random first name for sender and random word from lorem for domain
+        IP           - IP address - May be private
+        CC[:MC|V|DI] - Credit card - randomly generated CC, probably not valid
+        V[:#]        - A randomly generated sentence of # words long
+        PW[:#]       - Randomly generated password of # length
 
     Examples:
         ./$0 --table tbl_mail --columns id account_id to from subject message date read important --count 25 'I;I;E;E;V:5;V:25;D;B;B'
 EOF
 }
 
-my @names = qw/James Mary Robert Patricia John Jennifer Michael Linda David Elizabeth William Barbara Richard Susan Joseph Jessica Thomas Sarah Christopher Karen Charles Lisa Daniel Nancy Matthew Betty Anthony Sandra Mark Margaret Donald Ashley Steven Kimberly Andrew Emily Paul Donna Joshua Michelle Kenneth Carol Kevin Amanda Brian Melissa George Deborah Timothy Stephanie Ronald Dorothy Jason Rebecca Edward Sharon Jeffrey Laura Ryan Cynthia Jacob Amy Gary Kathleen Nicholas Angela Eric Shirley Jonathan Brenda Stephen Emma Larry Anna Justin Pamela Scott Nicole Brandon Samantha Benjamin Katherine Samuel Christine Gregory Helen Alexander Debra Patrick Rachel/;
+my (@first_names, @last_names, @lorem_ipsum);
 
-my @lorem = qw/Lorem ipsum dolor sit amet consectetur adipiscing elit Sed eget nibh velit Vivamus luctus molestie lorem ac rhoncus Donec sem leo molestie ac nisl vel blandit tempus est Proin lacinia volutpat metus nec interdum Aenean lacus odio blandit id nisi sit amet venenatis commodo ex Nam convallis tellus vel imperdiet hendrerit mauris tortor pellentesque nisi at molestie massa ante a erat In nec iaculis arcu Nullam ac auctor urna Cras id nisl varius pulvinar ante quis lacinia erat Mauris elementum lectus interdum convallis efficitur ante leo convallis diam a interdum est dui nec dui Proin mattis diam in metus venenatis at porta leo facilisis/;
+open my $fh_fn, '<', $first;
+    @first_names = <$fh_fn>;
+close $fh_fn;
+
+open my $fh_ln, '<', $last;
+    @last_names = <$fh_ln>;
+close $fh_ln;
+
+open my $fh_li, '<', $ipsum;
+    @lorem_ipsum = split / /, <$fh_li>;
+close $fh_li;
+
 
 my @data    = split /;/, shift;
 my $out_data;
 
+
+print scalar @first_names . " first names\n";
+print scalar @last_names  . " last names\n";
+print scalar @lorem_ipsum . " lorem ipsum words\n";
+
 for (1 .. $count) {
     $out_data = "INSERT INTO `$table` (`" . join('`, `', @columns) . "`) VALUES (";
 
+    my $data_count   = scalar @data;
+    my $column_count = scalar @columns;
+
+    if ($data_count != $column_count) {
+        die "Column count ($column_count) doesn't match the received indicator count ($data_count)\n";
+    }
+
     for (my $i=0; $i<scalar @data - 1; $i++) {
         my ($type, $args) = split /:/, $data[$i];
-        $args //= 1;
 
         if ($type eq 'V') {
             my $string;
+            my $word_count = $args ? $args : 25;
             for (1 .. $args) {
-                $string .= $lorem[int(rand(scalar @lorem)-1)] . ' ';
+                chomp (my $word = $lorem_ipsum[int(rand(scalar @lorem_ipsum)-1)]);
+                $string .= $word . ' ';
             }
             $out_data .= "'$string'";
         } elsif ($type =~ /[FL]/) {
-            my $name = $names[int(rand(scalar @names)-1)];
+            my $name = $type eq 'F'
+                ? $first_names[int(rand(scalar @first_names)-1)]
+                : $last_names[int(rand(scalar @last_names)-1)];
+                chomp $name;
             $out_data .= "'$name'";
         } elsif ($type eq 'B') {
             my $pick = int(rand(2)) ? 'True' : 'False';
             $out_data .= "'$pick'";
         } elsif ($type eq 'D') {
-            if ($args eq 'T') {
+            if ($args && $args eq 'T') {
                 $out_data .= "NOW()";
             } else {
                 my ($year, $month, $day, $hour, $min, $sec) = (int(rand(14)+2024),int(rand(12)+1),int(rand(28)+1),int(rand(23)+1),int(rand(59)+1),int(rand(59)+1));
@@ -86,7 +120,8 @@ for (1 .. $count) {
         } elsif ($type eq 'N') {
             $out_data .= "NULL";
         } elsif ($type eq 'I') {
-            my $num = int(rand(5000));
+            my $max = $args ? $args : 5000;
+            my $num = int(rand($max));
             $out_data .= "$num";
         } elsif ($type eq 'P') {
             my ($area, $district, $ending) = (int(rand(999)), int(rand(999)), int(rand(9999)));
@@ -103,9 +138,10 @@ for (1 .. $count) {
             my $ip = "$o1.$o2.$o3.$o4";
             $out_data .= "'$ip'";
         } elsif ($type eq 'E') {
-            my $sender = $names[int(rand(scalar @names)-1)];
-            my $domain = $lorem[int(rand(scalar @lorem)-1)];
-            my $email  = "$sender\@$domain.com";
+            chomp(my $sender = $first_names[int(rand(scalar @first_names)-1)]);
+            chomp($sender    = $last_names[int(rand(scalar @last_names)-1)]);
+            chomp(my $domain = $lorem_ipsum[int(rand(scalar @lorem_ipsum)-1)]);
+            my $email = "$sender\@$domain.com";
 
             $out_data .= "'$email'";
         } elsif ($type eq 'M') {
@@ -119,7 +155,9 @@ for (1 .. $count) {
             $out_data .= "'$cc'";
         } elsif ($type eq 'PW') {
             my $password;
-            for (1 .. 15) {
+            my $max = $args ? $args : 10;
+
+            for (1 .. $max) {
                 $password .= chr(int(rand(25)+65));
             }
             $out_data .= "'$password'";
